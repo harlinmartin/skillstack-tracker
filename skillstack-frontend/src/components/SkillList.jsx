d7493aab03f38052a6c05e3cd38470d6315798ef
harlinmartin <harlinmartin58@gmail.com>
2025-12-03 19:17:18 +0530

    add SkillList to render list of skills

---
+++ skillstack-frontend/src/components/SkillList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SkillCard from "./SkillCard";
+
+export default function SkillList({ skills, onUpdate, onDelete }) {
+  if (!skills || !skills.length) return <p>No skills yet. Add one!</p>;
+  return (
+    <div>
+      {skills.map(skill => (
+        <SkillCard key={skill.id} skill={skill} onUpdate={onUpdate} onDelete={onDelete} />
+      ))}
+    </div>
+  );
+}
